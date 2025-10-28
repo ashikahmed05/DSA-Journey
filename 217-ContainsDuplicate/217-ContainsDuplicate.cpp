cfcd73cb1b6d8078b7cfe1ff03d3ec474d14b757
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-10-28 18:21:51 +0530

    File updated at 10/28/2025, 6:21:51 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.cpp
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -1,10 +1,12 @@
-// Last updated: 10/28/2025, 6:18:55 PM
+// Last updated: 10/28/2025, 6:21:51 PM
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
 
         // Here i used the set tool for distinguish the unique element if duplicated.
-        set<int> setnum;
+
+        unordered_set<int> setnum;
+
         for (int i : nums){
             setnum.insert(i);
         }
