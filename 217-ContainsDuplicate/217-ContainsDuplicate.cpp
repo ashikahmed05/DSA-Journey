a3b6fd0fba43716e126d104cb4aa5acdf3684af2
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-10-28 18:35:23 +0530

    File updated at 10/28/2025, 6:35:22 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.cpp
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -1,9 +1,9 @@
-// Last updated: 10/28/2025, 6:27:40 PM
+// Last updated: 10/28/2025, 6:35:22 PM
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
 
-        // Here i used the set tool for distinguish the unique element if duplicated.
+        // Here i used the set tool for distinguish the unique element if duplicated. Time & Space Complexity O(n)
 
         unordered_set<int> setnum;
 
