d5d9696949bfa71b4d33e4828946e1ddeaa45401
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-10-28 17:46:33 +0530

    File updated at 10/28/2025, 5:46:32 PM

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,22 +1,19 @@
-// Last updated: 10/28/2025, 5:37:20 PM
+// Last updated: 10/28/2025, 5:46:32 PM
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         // This is my first LeetCode Problem solution, with time complexity O(n) and Space Complexity O(n).
-        // Here i used unordered_map for fast look up its key and value pair i have already visited through manual iteration .
+        // Here i used unordered_map for fast look up its key and value pair i have already visited.
+        
 
         unordered_map<int, int> ump;
-        vector<int> result;
 
         for (int i = 0; i < nums.size(); i++){
             int x = target-nums[i];
 
             if (ump.find(x) != ump.end()){
-
-                result.push_back(i);
-                result.push_back(ump[x]);
-                return result;
+                return {ump[x], i};
                 
 
             } else {
