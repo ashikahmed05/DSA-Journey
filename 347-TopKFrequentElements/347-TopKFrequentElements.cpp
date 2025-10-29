beedf3b737b82fb3b0b4753af608518edf9f3183
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-10-29 21:31:33 +0530

    File updated at 10/29/2025, 9:31:31 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.cpp
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -1,38 +1,39 @@
-// Last updated: 10/29/2025, 9:00:51 PM
+// Last updated: 10/29/2025, 9:31:31 PM
 class Solution {
 public:
 
-// Here i used unordered_set and map to get the maximum no occurance with time complexity O(n)
-    int maxFinder (unordered_set<int>& setnum, unordered_map<int, int>& mp){
-        int max = 0;
-        int maxNum;
-        for (int set : setnum){
-            if (mp[set] >= max){
-                max = mp[set];
-                maxNum = set;
-            } 
-
-        }
-        setnum.erase(maxNum);
-        return maxNum;
-
-
-    }
+// Here i used unordered_map to get the maximum no of occurance with the help of bucket sorting with time & space complexity O(n)
 
 
     vector<int> topKFrequent(vector<int>& nums, int& k) {
         unordered_map<int, int> mp;
-        unordered_set<int> setnum;
+        vector<vector<int>> bucket(nums.size() + 1);
         vector<int> result;
 
         for (int& s : nums){
             mp[s]++;
-            setnum.insert(s);
+        }
+        for (auto& pair : mp){
+            
+            bucket[pair.second].push_back(pair.first);
+        }
+        for (int i = nums.size(); i >= 0 ; i--){
+            if (!bucket[i].empty()){
+                for (int x : bucket[i]){
+                    if (k > result.size()){
+                        result.push_back(x);
+                        
+
+                    }
+
+                    
+
+                }
+                
+            }
         }
 
-        while(k--){
-            result.push_back(maxFinder(setnum, mp));
-        }
+
         return result;
 
 
