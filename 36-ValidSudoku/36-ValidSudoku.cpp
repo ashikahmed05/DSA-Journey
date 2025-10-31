82e12ba683d530eb85e2fc791e2719435adb6e23
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-10-31 17:25:30 +0530

    File updated at 10/31/2025, 5:25:28 PM

--- 36-ValidSudoku/36-ValidSudoku.cpp
+++ 36-ValidSudoku/36-ValidSudoku.cpp
@@ -1,68 +1,37 @@
-// Last updated: 10/31/2025, 4:05:16 PM
+// Last updated: 10/31/2025, 5:25:28 PM
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_map<char, int>> main(9);
+
+        // Here i use set for determining the existance of the same no and its time complexity is O(n).
         
+        map<pair<int, int>, unordered_set<char>> grid;
 
-        for (int j =0; j < 9; j++){
-            unordered_map<char, int> mp;
-            for (int i =0; i < 9; i++){
+        for (int i = 0; i < 9 ; i++){
+            unordered_set<char> row, col;
+            for (int j = 0; j < 9; j++){
                 char x = board[i][j];
-                if (x != '.'){
-                    mp[x]++;
-                }
-            }
-            for (auto& pair : mp){
-                if (pair.second > 1) {
+                char y = board[j][i];
+                pair<int, int> temp = {i/3, j/3};
+
+
+                if (row.count(x) || col.count(y) || grid[temp].count(x)){
                     return false;
-                }
-            }
-            
-        }
-        for (int j =0; j < 9; j++){
-            unordered_map<char, int> mp;
-            for (int i =0; i < 9; i++){
-                char x = board[j][i];
-                if (x != '.'){
-                    mp[x]++;
-                }
-                if ( i < 3 && j < 3 && x != '.'){
-                    main[0][x]++;
-                } else if ( i < 6 && i >= 3 && j < 3 && x != '.'){
-                    main[1][x]++;
-                } else if ( i < 9 && i >= 6 && j < 3 && x != '.'){
-                    main[2][x]++;
-                } else if ( i < 6 && i >= 3 && j < 6 && j >= 3 && x != '.'){
-                    main[4][x]++;
-                } else if ( i < 9 && i >= 6 && j < 6 && j >= 3 && x != '.'){
-                    main[5][x]++;
-                } else if ( i < 3 && j < 6 && j >= 3 && x != '.'){
-                    main[3][x]++;
-                } else if ( i < 6 && i >= 3 && j < 9 && j >= 6 && x != '.'){
-                    main[6][x]++;
-                } else if ( i < 9 && i >= 6 && j < 9 && j >= 6 && x != '.'){
-                    main[7][x]++;
-                } else if ( i < 3 && j < 9 && j >= 6 && x != '.'){
-                    main[8][x]++;
-                }
-            }
-            for (auto& pair : mp){
-                if (pair.second > 1) {
-                    return false;
-                }
-            }
-            for (auto& s : main){
-                for (auto& pair : s) {
-                    if (pair.second > 1){
-                        return false;
+                } else {
+                    if(x != '.'){
+                        row.insert(x);
+                        grid[temp].insert(x);
                     }
+                    if(y != '.'){
+                        col.insert(y);
+                    }
+                    
+                    
+                    
+
                 }
             }
-            
         }
-
-
 
         return true;
         
