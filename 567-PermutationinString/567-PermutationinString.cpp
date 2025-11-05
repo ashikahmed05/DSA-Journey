d4bf6b0ecde7183ece7520e9ede5161448957d70
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-11-05 11:50:47 +0530

    Create 567-PermutationinString

---
+++ 567-PermutationinString/567-PermutationinString.cpp
@@ -0,0 +1,20 @@
+// Last updated: 11/5/2025, 11:50:47 AM
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int> fs1(26, 0), fs2(26, 0);
+        if (s1.size() > s2.size()) return false;
+        int l = 0;
+        for (char c : s1) fs1[c-'a']++;
+
+        for (int r = 0; r < s2.size(); r++){
+            fs2[s2[r] - 'a']++;
+
+            if (r - l + 1 < s1.size()) continue;
+            if (fs1 == fs2) return true;
+            fs2[s2[l] - 'a']--;
+            l++;
+        }       
+        return false;
+    }
+};
