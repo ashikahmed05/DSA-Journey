75561df98d4c85256a137349f649f0582b164474
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-11-08 18:58:16 +0530

    File updated at 11/8/2025, 6:58:15 PM

--- 853-CarFleet/853-CarFleet.cpp
+++ 853-CarFleet/853-CarFleet.cpp
@@ -1,7 +1,8 @@
-// Last updated: 11/8/2025, 6:47:31 PM
+// Last updated: 11/8/2025, 6:58:15 PM
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        // Here i used deque for iterating the position , time pair from back with time complexity O(nlogn) & SC O(n);
         int ans = 1;
         double time;
         if(speed.size() == 0) return 0;
