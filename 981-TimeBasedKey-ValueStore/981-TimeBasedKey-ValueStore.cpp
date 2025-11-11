333059ed6f1d9f0484ed5e57495c58e7fa76bf38
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-11-11 15:24:44 +0530

    Create 981-TimeBasedKey-ValueStore

---
+++ 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.cpp
@@ -0,0 +1,29 @@
+// Last updated: 11/11/2025, 3:24:43 PM
+class TimeMap {
+public:
+    unordered_map<string, map<int, string>> db;
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        
+        db[key][timestamp] = value;
+        
+    }
+    
+    string get(string key, int timestamp) {
+
+        auto res = db[key].upper_bound(timestamp);
+        if (res == db[key].begin()) return "";
+        res--;
+
+        return res -> second;
+        
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
