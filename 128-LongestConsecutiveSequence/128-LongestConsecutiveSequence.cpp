0fbe9dfc652272178cfd2aefb4e8791a41b468f9
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-11-23 19:19:29 +0530

    File updated at 11/23/2025, 7:19:30 PM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.cpp
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.cpp
@@ -1,30 +1,25 @@
-// Last updated: 11/23/2025, 12:49:53 AM
+// Last updated: 11/23/2025, 7:19:30 PM
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
 
-        // Here i checked the pre existed no to find the inial consecutive no and so on with time complexity O(n). 
-        unordered_set<int> set;
-        int Max = 0;
+        // Here i checked the pre existed no to find the inial consecutive no and so on with time complexity O(n).
+        unordered_set<int> number(nums.begin(), nums.end());
+        int maximum = 0;
 
-        for (int i : nums){
-            set.insert(i);
-        }
-        for (int s : set){
-            int x = s;
-            int y = 0;
-            if (!set.count(s-1)){               
-                while(set.count(x++)){
-                    y++;
+        for(int s : number){
+            if (number.find(s - 1) == number.end()){
+                int length = 1;
+                while(number.find(s + length) != number.end()){
+                    length++;
+
                 }
-                Max = max(Max, y);
-            } 
-            
+                maximum = max(length, maximum);
+
+            }
         }
-
-
-        
-        return Max;
+        return maximum;
         
     }
+    
 };
