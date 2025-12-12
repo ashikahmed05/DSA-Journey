6980a9af7290000b875b46b474f07f4cecd7fff9
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-12-12 14:38:43 +0530

    File updated at 12/12/2025, 2:38:43 PM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.cpp
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.cpp
@@ -1,13 +1,21 @@
-// Last updated: 12/12/2025, 1:18:31 PM
+// Last updated: 12/12/2025, 2:38:43 PM
 1class Solution {
 2public:
 3    int findDuplicate(vector<int>& nums) {
-4        unordered_map<int,int>db;
-5        for(int i = 0; i < nums.size(); i++ ){
-6            auto it = db.insert({nums[i], i});
-7            if (it.second == false) return nums[i];
-8        }
-9        return 0;
-10        
-11    }
-12};
+4        int slow = nums[0];
+5        int fast = nums[0];
+6        do{
+7            slow = nums[slow];
+8            fast = nums[nums[fast]];
+9        } while(slow != fast);
+10
+11        fast = nums[0];
+12
+13        while(slow != fast){
+14            fast = nums[fast];
+15            slow = nums[slow];
+16        }
+17        return slow;
+18        
+19    }
+20};
