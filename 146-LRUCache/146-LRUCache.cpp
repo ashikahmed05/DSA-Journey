1ab111d8362fa50b041ee03383f3ffbaa8fc482a
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-12-13 10:17:24 +0530

    File updated at 12/13/2025, 10:17:24 AM

--- 146-LRUCache/146-LRUCache.cpp
+++ 146-LRUCache/146-LRUCache.cpp
@@ -1,47 +1,48 @@
-// Last updated: 12/13/2025, 9:59:56 AM
+// Last updated: 12/13/2025, 10:17:24 AM
 1class LRUCache {
-2public:
-3    int maxcap; 
-4    unordered_map<int,list<pair<int,int>>::iterator>map;
-5    list<pair<int,int>>dll;
-6
-7    
-8    LRUCache(int capacity) {
-9        maxcap = capacity;
-10    }
-11    
-12    int get(int key) {
-13        if(map.find(key) == map.end()){
-14            return -1;
-15        }
-16        auto it = map[key];
-17        dll.splice(dll.begin(), dll, it);
-18        return it->second;
-19        
-20    }
-21    
-22    void put(int key, int value) {
-23        if(map.find(key) != map.end()){
-24            auto it = map[key];
-25            it->second = value;
-26            dll.splice(dll.begin(), dll, it);
-27            return;
-28        }
-29        if (map.size() >= maxcap){
-30            if (maxcap == 0) return;
-31            map.erase(dll.back().first);
-32            dll.pop_back();           
-33        }
-34
-35        dll.push_front({key, value});
-36        map[key] = dll.begin();
-37        
-38    }
-39};
-40
-41/**
-42 * Your LRUCache object will be instantiated and called as such:
-43 * LRUCache* obj = new LRUCache(capacity);
-44 * int param_1 = obj->get(key);
-45 * obj->put(key,value);
-46 */
+2    // Here i am using doubly linked list (std::list) with TC O(1).
+3public:
+4    int maxcap; 
+5    unordered_map<int,list<pair<int,int>>::iterator>map;
+6    list<pair<int,int>>dll;
+7
+8    
+9    LRUCache(int capacity) {
+10        maxcap = capacity;
+11    }
+12    
+13    int get(int key) {
+14        if(map.find(key) == map.end()){
+15            return -1;
+16        }
+17        auto it = map[key];
+18        dll.splice(dll.begin(), dll, it);
+19        return it->second;
+20        
+21    }
+22    
+23    void put(int key, int value) {
+24        if(map.find(key) != map.end()){
+25            auto it = map[key];
+26            it->second = value;
+27            dll.splice(dll.begin(), dll, it);
+28            return;
+29        }
+30        if (map.size() >= maxcap){
+31            if (maxcap == 0) return;
+32            map.erase(dll.back().first);
+33            dll.pop_back();           
+34        }
+35
+36        dll.push_front({key, value});
+37        map[key] = dll.begin();
+38        
+39    }
+40};
+41
+42/**
+43 * Your LRUCache object will be instantiated and called as such:
+44 * LRUCache* obj = new LRUCache(capacity);
+45 * int param_1 = obj->get(key);
+46 * obj->put(key,value);
+47 */
