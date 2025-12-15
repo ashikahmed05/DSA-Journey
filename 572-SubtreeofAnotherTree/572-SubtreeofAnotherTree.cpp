d5f4ac9e2383e6d523798dd1cba3c0778b60e7fb
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-12-15 11:41:31 +0530

    File updated at 12/15/2025, 11:41:30 AM

--- 572-SubtreeofAnotherTree/572-SubtreeofAnotherTree.cpp
+++ 572-SubtreeofAnotherTree/572-SubtreeofAnotherTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 12/15/2025, 11:12:08 AM
+// Last updated: 12/15/2025, 11:41:30 AM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -12,23 +12,21 @@
 11 */
 12class Solution {
 13public:
-14    bool dfs(TreeNode* root, TreeNode* subRoot) {
-15        if(!root || !subRoot){
-16            if (!root && !subRoot){
-17                 return true;
-18            } else return false;
-19
-20        }
-21        if (root->val != subRoot->val) return false;
-22
-23        return dfs(root->left, subRoot->left) && dfs(root->right, subRoot->right);
-24        
-25    }
-26    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-27        if(!root) return false;
-28        if(dfs(root,subRoot)) return true;     
-29        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
-30        
-31        
-32    }
-33};
+14    string s = "empty";
+15    string stringify(TreeNode* root){
+16        if(!root){
+17            return "#";
+18        }
+19        string value;
+20        if(s != "true")value = stringify(root->left) + "L" + stringify(root->right) + "R" + to_string(root->val);
+21        if ( s != "empty") s = s == value ? "true" : s;
+22        
+23        return s == "true" ? s : value;
+24    }
+25    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+26        s = stringify(subRoot);
+27        stringify(root);
+28        return s=="true";
+29        
+30    }
+31};
