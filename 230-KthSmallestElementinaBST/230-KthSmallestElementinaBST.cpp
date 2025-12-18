1d7b92ac35cbccd70881e0714a4b81242ca74b0b
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-12-18 11:44:36 +0530

    File updated at 12/18/2025, 11:44:33 AM

--- 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.cpp
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.cpp
@@ -1,4 +1,4 @@
-// Last updated: 12/18/2025, 11:37:21 AM
+// Last updated: 12/18/2025, 11:44:33 AM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -10,22 +10,23 @@
 9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 10 * };
 11 */
-12class Solution {
-13public:
-14    void dfs(TreeNode* root, vector<int>& res){
-15        if(!root) return;
-16        dfs(root->left, res);
-17        res.push_back(root->val);
-18        dfs(root->right, res);
-19
+12 // here i am using bst property of sort using dfs with TC O(n) & SC O(h);
+13class Solution {
+14public:
+15    void dfs(TreeNode* root, vector<int>& res, int k){
+16        if(!root) return;
+17        dfs(root->left, res, k);
+18        if(res.size() < k)res.push_back(root->val);
+19        dfs(root->right, res, k);
 20
 21
-22    }
-23    int kthSmallest(TreeNode* root, int k) {
-24        vector <int> res;
-25        dfs(root, res);
-26        return res[k - 1];
-27        
-28    }
-29};
-30
+22
+23    }
+24    int kthSmallest(TreeNode* root, int k) {
+25        vector <int> res;
+26        dfs(root, res, k);
+27        return res[k - 1];
+28        
+29    }
+30};
+31
