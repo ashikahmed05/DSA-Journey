afd1bd89c7fefeb29fc2175e181860b1005de4f9
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-12-19 17:03:20 +0530

    File updated at 12/19/2025, 5:03:18 PM

--- 703-KthLargestElementinaStream/703-KthLargestElementinaStream.cpp
+++ 703-KthLargestElementinaStream/703-KthLargestElementinaStream.cpp
@@ -1,28 +1,29 @@
-// Last updated: 12/19/2025, 4:57:50 PM
+// Last updated: 12/19/2025, 5:03:18 PM
 1class KthLargest {
 2public:
-3    priority_queue<int, vector<int>, greater<int>> heap;
-4    int k;
-5
-6    KthLargest(int k, vector<int>& nums) {
-7        this->k = k;
-8        for(int i : nums){
-9            heap.push(i);
-10            if(heap.size() > k) heap.pop();
-11        }
-12        
-13    }
-14    
-15    int add(int val) {
-16        heap.push(val);
-17        if(heap.size() > k)heap.pop();
-18        return heap.top();
-19        
-20    }
-21};
-22
-23/**
-24 * Your KthLargest object will be instantiated and called as such:
-25 * KthLargest* obj = new KthLargest(k, nums);
-26 * int param_1 = obj->add(val);
-27 */
+3// here i use min heap with TC O(mlogn) and SC O(k)
+4    priority_queue<int, vector<int>, greater<int>> heap;
+5    int k;
+6
+7    KthLargest(int k, vector<int>& nums) {
+8        this->k = k;
+9        for(int i : nums){
+10            heap.push(i);
+11            if(heap.size() > k) heap.pop();
+12        }
+13        
+14    }
+15    
+16    int add(int val) {
+17        heap.push(val);
+18        if(heap.size() > k)heap.pop();
+19        return heap.top();
+20        
+21    }
+22};
+23
+24/**
+25 * Your KthLargest object will be instantiated and called as such:
+26 * KthLargest* obj = new KthLargest(k, nums);
+27 * int param_1 = obj->add(val);
+28 */
