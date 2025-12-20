58b8e4ecddde8ffb9366e483e5c7d2975bf3f97f
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-12-20 10:44:36 +0530

    File updated at 12/20/2025, 10:44:35 AM

--- 1046-LastStoneWeight/1046-LastStoneWeight.cpp
+++ 1046-LastStoneWeight/1046-LastStoneWeight.cpp
@@ -1,18 +1,18 @@
-// Last updated: 12/20/2025, 10:37:27 AM
+// Last updated: 12/20/2025, 10:44:35 AM
 1class Solution {
-2public:
-3    int lastStoneWeight(vector<int>& stones) {
-4        priority_queue<int> pq;
-5
-6        for(int i : stones) pq.push(i);
-7
-8        while(pq.size() > 1){
-9            int s1 = pq.top();            
-10            pq.pop();
-11            int s2 = pq.top();
-12            pq.pop();
-13            if(s2 != s1) pq.push(s1 - s2);
-14            
+2    // I am taking max heap with TC O(nlogn) & SC O(n);
+3public:
+4    int lastStoneWeight(vector<int>& stones) {
+5        priority_queue<int> pq;
+6
+7        for(int i : stones) pq.push(i);
+8
+9        while(pq.size() > 1){
+10            int s1 = pq.top();            
+11            pq.pop();
+12            int s2 = pq.top();
+13            pq.pop();
+14            if(s2 != s1) pq.push(s1 - s2);            
 15        }
 16        if (pq.empty()) return 0;
 17        return pq.top();
