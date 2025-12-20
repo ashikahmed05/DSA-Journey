f288820cc7249cd0f4b006b081125f45f0b7d034
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-12-20 12:49:39 +0530

    File updated at 12/20/2025, 12:49:38 PM

--- 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.cpp
+++ 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.cpp
@@ -1,22 +1,23 @@
-// Last updated: 12/20/2025, 12:45:06 PM
+// Last updated: 12/20/2025, 12:49:38 PM
 1class Solution {
-2public:
-3    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-4        priority_queue<pair<int, int>> pq;
-5        vector<vector<int>> res;
-6        for( int i = 0; i < points.size(); i++){
-7            int dist = (points[i][0])*(points[i][0]) + (points[i][1])*(points[i][1]);
-8            pq.push({dist, i});
-9
-10            if(pq.size() > k){
-11                pq.pop();
-12            }
-13        }
-14        while(!pq.empty()){
-15            res.push_back(points[pq.top().second]);
-16            pq.pop();
-17        }
-18        return res;
-19        
-20    }
-21};
+2    // Here i took max heap with TC O(klogn) and SC O(k);
+3public:
+4    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+5        priority_queue<pair<int, int>> pq;
+6        vector<vector<int>> res;
+7        for( int i = 0; i < points.size(); i++){
+8            int dist = (points[i][0])*(points[i][0]) + (points[i][1])*(points[i][1]);
+9            pq.push({dist, i});
+10
+11            if(pq.size() > k){
+12                pq.pop();
+13            }
+14        }
+15        while(!pq.empty()){
+16            res.push_back(points[pq.top().second]);
+17            pq.pop();
+18        }
+19        return res;
+20        
+21    }
+22};
