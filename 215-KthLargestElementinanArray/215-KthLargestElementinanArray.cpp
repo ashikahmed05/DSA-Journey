b60b2f8226879b3b54ca60a4560724c2a0984131
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-12-21 12:20:55 +0530

    File updated at 12/21/2025, 12:20:54 PM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.cpp
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.cpp
@@ -1,14 +1,15 @@
-// Last updated: 12/21/2025, 12:20:12 PM
+// Last updated: 12/21/2025, 12:20:54 PM
 1class Solution {
-2public:
-3    int findKthLargest(vector<int>& nums, int k) {
-4        priority_queue<int, vector<int>, greater<int>> pq;
-5
-6        for(int i : nums){
-7            pq.push(i);
-8            if(pq.size() > k) pq.pop();
-9        }
-10        return pq.top();
-11        
-12    }
-13};
+2    // Here i am using min heap with TC O(nlogk) & SC O(k);
+3public:
+4    int findKthLargest(vector<int>& nums, int k) {
+5        priority_queue<int, vector<int>, greater<int>> pq;
+6
+7        for(int i : nums){
+8            pq.push(i);
+9            if(pq.size() > k) pq.pop();
+10        }
+11        return pq.top();
+12        
+13    }
+14};
