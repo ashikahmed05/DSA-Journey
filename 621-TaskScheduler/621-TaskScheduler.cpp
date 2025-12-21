095ab9cc6a8b26230e5043a8a93168ebff2a2cc3
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-12-21 17:20:38 +0530

    File updated at 12/21/2025, 5:20:37 PM

--- 621-TaskScheduler/621-TaskScheduler.cpp
+++ 621-TaskScheduler/621-TaskScheduler.cpp
@@ -1,30 +1,31 @@
-// Last updated: 12/21/2025, 2:50:24 PM
+// Last updated: 12/21/2025, 5:20:37 PM
 1class Solution {
-2public:
-3    int leastInterval(vector<char>& tasks, int n) {
-4        vector<int> fq(26,0);
-5        for(char c : tasks){
-6            fq[c - 'A']++;
-7        }
-8        priority_queue<int>pq;
-9        for(int i : fq){
-10            if(i != 0) pq.push(i);
-11        }
-12        queue<pair<int,int>>que;
-13        int time = 0;
-14        while(!pq.empty() || !que.empty()){
-15            time++;
-16            if(!pq.empty()){
-17                if(pq.top() - 1 != 0){
-18                    que.push({pq.top() - 1, time + n});               
-19                }
-20                pq.pop();
-21            } 
-22            if(!que.empty() && que.front().second <= time){
-23                pq.push(que.front().first);
-24                que.pop();              
-25            }
-26        }
-27        return time;
-28    }
-29};
+2    // Greedy rule applied with TC O(N) and SC O(log26)
+3public:
+4    int leastInterval(vector<char>& tasks, int n) {
+5        vector<int> fq(26,0);
+6        for(char c : tasks){
+7            fq[c - 'A']++;
+8        }
+9        priority_queue<int>pq;
+10        for(int i : fq){
+11            if(i != 0) pq.push(i);
+12        }
+13        queue<pair<int,int>>que;
+14        int time = 0;
+15        while(!pq.empty() || !que.empty()){
+16            time++;
+17            if(!pq.empty()){
+18                if(pq.top() - 1 != 0){
+19                    que.push({pq.top() - 1, time + n});               
+20                }
+21                pq.pop();
+22            } 
+23            if(!que.empty() && que.front().second <= time){
+24                pq.push(que.front().first);
+25                que.pop();              
+26            }
+27        }
+28        return time;
+29    }
+30};
