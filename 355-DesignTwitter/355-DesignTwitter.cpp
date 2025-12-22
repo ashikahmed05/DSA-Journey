5e3ab362cd37c86ad76f5b1912bbb5c117bde694
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2025-12-22 14:07:05 +0530

    File updated at 12/22/2025, 2:07:03 PM

--- 355-DesignTwitter/355-DesignTwitter.cpp
+++ 355-DesignTwitter/355-DesignTwitter.cpp
@@ -1,70 +1,71 @@
-// Last updated: 12/22/2025, 1:47:13 PM
+// Last updated: 12/22/2025, 2:07:03 PM
 1class Twitter {
 2public:
-3    unordered_map<int, vector<pair<int, int>>> tweets;
-4    unordered_map<int, unordered_set<int>> users;
-5    int time = 0;
-6    Twitter() {
-7        
-8    }
-9    
-10    void postTweet(int userId, int tweetId) {
-11        time++;
-12        tweets[userId].push_back({time*(-1), tweetId});
-13        
-14    }
-15    
-16    vector<int> getNewsFeed(int userId) {
-17        
-18        priority_queue<pair<int, int>>pq;
-19        int sz = tweets[userId].size();
-20
-21        if (sz > 0){
-22            for(int j = sz - 1; j >= max(0, sz-10); j--){
-23                pq.push(tweets[userId][j]);
-24                if(pq.size() > 10) pq.pop();
-25            }
-26        }
-27
+3// Here used to scan top 10 latest tweets using heap.
+4    unordered_map<int, vector<pair<int, int>>> tweets;
+5    unordered_map<int, unordered_set<int>> users;
+6    int time = 0;
+7    Twitter() {
+8        
+9    }
+10    
+11    void postTweet(int userId, int tweetId) {
+12        time++;
+13        tweets[userId].push_back({time, tweetId});
+14        
+15    }
+16    
+17    vector<int> getNewsFeed(int userId) {
+18        
+19        priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+20        int sz = tweets[userId].size();
+21
+22        if (sz > 0){
+23            for(int j = sz - 1; j >= max(0, sz-10); j--){
+24                pq.push(tweets[userId][j]);
+25                if(pq.size() > 10) pq.pop();
+26            }
+27        }
 28
-29        for(int i : users[userId]){
-30            int lz = tweets[i].size();
-31
-32            for(int j = max(0, lz - 1); j >= max(0, lz - 10) && lz != 0; j--){
-33
-34                pq.push(tweets[i][j]);
-35                if(pq.size() > 10) pq.pop();
-36            }           
-37        } 
-38        int size = pq.size();
-39        vector<int>res(size);
-40
-41        while(!pq.empty()){
-42            size--;
-43            res[size] = pq.top().second;
-44            pq.pop();
-45
-46        }
-47        return res;
-48        
-49    }
-50    
-51    void follow(int followerId, int followeeId) {
-52        if(followerId != followeeId) users[followerId].insert(followeeId);
-53        
-54    }
-55    
-56    void unfollow(int followerId, int followeeId) {
-57        users[followerId].erase(followeeId);
-58        
-59    }
-60};
-61
-62/**
-63 * Your Twitter object will be instantiated and called as such:
-64 * Twitter* obj = new Twitter();
-65 * obj->postTweet(userId,tweetId);
-66 * vector<int> param_2 = obj->getNewsFeed(userId);
-67 * obj->follow(followerId,followeeId);
-68 * obj->unfollow(followerId,followeeId);
-69 */
+29
+30        for(int i : users[userId]){
+31            int lz = tweets[i].size();
+32
+33            for(int j = max(0, lz - 1); j >= max(0, lz - 10) && lz != 0; j--){
+34
+35                pq.push(tweets[i][j]);
+36                if(pq.size() > 10) pq.pop();
+37            }           
+38        } 
+39        int size = pq.size();
+40        vector<int>res(size);
+41
+42        while(!pq.empty()){
+43            size--;
+44            res[size] = pq.top().second;
+45            pq.pop();
+46
+47        }
+48        return res;
+49        
+50    }
+51    
+52    void follow(int followerId, int followeeId) {
+53        if(followerId != followeeId) users[followerId].insert(followeeId);
+54        
+55    }
+56    
+57    void unfollow(int followerId, int followeeId) {
+58        users[followerId].erase(followeeId);
+59        
+60    }
+61};
+62
+63/**
+64 * Your Twitter object will be instantiated and called as such:
+65 * Twitter* obj = new Twitter();
+66 * obj->postTweet(userId,tweetId);
+67 * vector<int> param_2 = obj->getNewsFeed(userId);
+68 * obj->follow(followerId,followeeId);
+69 * obj->unfollow(followerId,followeeId);
+70 */
