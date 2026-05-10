57a9e798349f322e5b2f3304dc49a963deab4653
Ashik Ahmed <154865503+trueashik@users.noreply.github.com>
2026-05-11 00:42:46 +0530

    Update: 2855. Maximum Number of Jumps to Reach the Last Index (C++)

--- Daily Problems/Array/Medium/2855-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ Daily Problems/Array/Medium/2855-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -2,8 +2,8 @@
 // Problem: 2855. Maximum Number of Jumps to Reach the Last Index
 // Difficulty: Medium
 // Topics: Array, Dynamic Programming
-// Runtime: 23 ms (Beats 43.5%)
-// Memory: 68.5 MB (Beats 40.1%)
+// Runtime: 24 ms (Beats 33.9%)
+// Memory: 68.6 MB (Beats 40.1%)
 // Submitted: May 11, 2026
 // Link: https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/
 // ═══════════════════════════════════════════════════════
