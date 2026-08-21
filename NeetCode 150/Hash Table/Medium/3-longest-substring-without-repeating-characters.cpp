68227b1fa720033b41c20c921a625592f6bf153a
Ashik Ahmed <154865503+trueashik@users.noreply.github.com>
2026-08-21 14:22:40 +0530

    Update: 3. Longest Substring Without Repeating Characters (C++)

--- NeetCode 150/Hash Table/Medium/3-longest-substring-without-repeating-characters.cpp
+++ NeetCode 150/Hash Table/Medium/3-longest-substring-without-repeating-characters.cpp
@@ -2,8 +2,8 @@
 // Problem: 3. Longest Substring Without Repeating Characters
 // Difficulty: Medium
 // Topics: Hash Table, String, Sliding Window
-// Runtime: 8 ms (Beats 67.4%)
-// Memory: 18.5 MB (Beats 38.7%)
+// Runtime: 11 ms (Beats 59.9%)
+// Memory: 18.3 MB (Beats 40.3%)
 // Submitted: Aug 21, 2026
 // Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 // ═══════════════════════════════════════════════════════
@@ -11,7 +11,7 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> unik(127);
+        vector<int> unik(128);
         int ans = 0;
         int n = s.size();
 
@@ -19,10 +19,10 @@ public:
         int r = 0;
 
         while(r < n){
-            int c1 = s[r] - '\0';
+            int c1 = s[r];
             
             while(unik[c1] > 0 && l <= r){
-                int c2 = s[l] - '\0';
+                int c2 = s[l];
                 unik[c2]--;
                 l++;
             }
